ed1750d2deb39d61430d01b57f7b9d47f07a430e
FigoHunter <zhaoyifei19950308@hotmail.com>
2024-04-15 13:12:50 +0800

    packages

---
+++ packages/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='bridge_service',
+    version='0.1.0',
+    packages=find_packages(),
+    license='MIT',
+    author='figo',
+    description='figo',
+    install_requires=['flask'],
+    tests_require=[]
+)
