462e6f6e73ad93f034dd6805fcfc170ef1d98fe2
FigoHunter <zhaoyifei19950308@hotmail.com>
2024-09-12 17:07:48 +0800

    client

--- _base.py
+++ _base.py
@@ -5,29 +5,45 @@ import traceback
 from . import utils
 
 class BridgeServiceClient:
-    def __init__(self, name, address='127.0.0.1', port=utils.PORT):
+    def __init__(self, name, address=None, port=None):
+        if address is None:
+            address = '127.0.0.1'
+        if port is None:
+            port = utils.PORT
         self.name = name
         self.address = address
         self.port = port
         self._action_registry = {}
-        utils.PORT = port
-        self._thread = threading.Thread(target=self._heartbeat, daemon=True)
-        self._thread.start()
+        self._thread = None
+        self._stopped = True
+        self._wait_lock = threading.Event()
+        self._start_wait_lock = threading.Event()
+        self._start_wait_lock.set()
+        self._request_lock = threading.RLock()
         
+
     def _heartbeat(self):
         while True:
+            if self._stopped:
+                break
+            self._request_lock.acquire()
             try:
                 response = requests.post(f'http://{self.address}:{self.port}/bridge-service/retrieve', json={'to_id': self.name},timeout=utils.TIMEOUT)
                 if response.status_code != 200:
                     raise Exception(f'Failed to retrieve actions for {self.name}')
                 self._parse_response(response)
+                self._start_wait_lock.set()
             except Exception as e:
                 traceback.print_exception(type(e), e, e.__traceback__)
-                time.sleep(5)
-            time.sleep(0.1)
+                time.sleep(3)
+                self._start_wait_lock.set()
+            self._request_lock.release()
+            time.sleep(0.2)
 
     def _parse_response(self,response:requests.Response):
-        for result in response.json():
+        if len(response.json()['data']) == 0:
+            return
+        for result in response.json()['data']:
             try:
                 action = result.get('action','')
                 args = result.get('args',[])
@@ -37,11 +53,31 @@ class BridgeServiceClient:
                     raise Exception(f'No action found for {action}')
             except Exception as e:
                 traceback.print_exception(type(e), e, e.__traceback__)
+        self._wait_lock.set()
 
-    def send_action(self,to_client,action,args=[]):
-        response = requests.post(f'http://{self.address}:{self.port}/bridge-service/register', json={'to_id':to_client,'from_id': self.name, 'action': action, 'args': args},timeout=utils.TIMEOUT)
-        if response.status_code != 200:
-            raise Exception(f'Failed to send action {action} with args {args}')
+    def connect(self, daemon=True):
+        if self._thread is None:
+            self._thread = threading.Thread(target=self._heartbeat, daemon=daemon)
+            self._stopped = False
+            self._thread.start()
+
+    def send_action(self,to_client,action,*args, wait=False, timeout=10):
+        if wait and not self._stopped:
+            self._wait_lock.clear()
+            self._start_wait_lock.clear()
+            self._start_wait_lock.wait()
+        try:
+            self._request_lock.acquire()
+            response = requests.post(f'http://{self.address}:{self.port}/bridge-service/register', json={'to_id':to_client,'from_id': self.name, 'action': action, 'args': list(args)},timeout=utils.TIMEOUT)
+            if response.status_code != 200:
+                raise Exception(f'Failed to send action {action} with args {args}')
+            self._request_lock.release()        
+        except Exception as e:
+            self._request_lock.release()
+            raise e
+        if wait and not self._stopped:
+            self._wait_lock.wait(timeout)
+
 
     def action(self, key=None):
         def decorator(func):
@@ -52,4 +88,11 @@ class BridgeServiceClient:
             return func
         return decorator
 
+    def stop(self):
+        self._stopped = True
+
+    def send_clear(self):
+        response = requests.post(f'http://{self.address}:{self.port}/bridge-service/clear', json={'to_id': self.name},timeout=utils.TIMEOUT)
+        if response.status_code != 200:
+            raise Exception(f'Failed to clear actions for {self.name}')
 
--- utils.py
+++ utils.py
@@ -1,10 +1,11 @@
 import uuid
 from functools import wraps
-
+from figo_common import environ
 
 PORT = 42443
 TIMEOUT = 5
-
+VAR_ADDR = 'BRIDGE_SERVICE_ADDR'
+VAR_PORT = 'BRIDGE_SERVICE_PORT'
 
 class BridgeAction(dict):
     def __init__(self, from_id, to_id, action, args):
@@ -30,3 +31,12 @@ class BridgeAction(dict):
     @property
     def args(self) -> list:
         return self['args']
+
+def set_env_var(addr=None, port=None):
+    if addr is not None:
+        environ.setEnvVar(VAR_ADDR, addr)
+    if port is not None:
+        environ.setEnvVar(VAR_PORT, port)
+
+def get_env_var():
+    return environ.getEnvVar(VAR_ADDR, None), environ.getEnvVar(VAR_PORT, None)
