4a8f221b14da72ca54f0fa0989079f4f58c7cc81
FigoHunter <zhaoyifei19950308@hotmail.com>
2024-09-12 17:07:20 +0800

    server

--- packages/bridge_service/flask/__init__.py
+++ packages/bridge_service/flask/__init__.py
@@ -13,6 +13,15 @@ if utils.DEBUG:
 else:
     log_level = logging.INFO
 
+def _format_response(success=True, message='', data=None):
+    return jsonify(
+        {
+            'success': success,
+            'message': message,
+            'data': data
+        }
+    )
+
 @app.route('/')
 def hello():
     app.logger.info('Hello route was accessed')
@@ -26,12 +35,17 @@ def register():
     args = request.json.get('args')
     core.BridgeService.instance().register(from_id, to_id, action, args)
     app.logger.info(f'{from_id} registered action {action} for {to_id}')
-    return 'Registered'
+    return _format_response(success=True, message='Registered')
 
 @app.route('/bridge-service/retrieve', methods=['POST'])
 def retrieve():
     to_id = request.json.get('to_id')
     actions = core.BridgeService.instance().pop_actions(to_id)
     app.logger.info(f'{to_id} retrieved {len(actions)} actions')
-    return jsonify(actions)
+    return _format_response(success=True, message='Retrieved', data=actions)
 
+@app.route('/bridge-service/clear', methods=['POST'])
+def clear():
+    to_id = request.json.get('to_id')
+    actions = core.BridgeService.instance().pop_actions(to_id)
+    return _format_response(success=True, message='Cleared', data=actions)
