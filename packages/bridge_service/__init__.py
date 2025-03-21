75e4c34e0479227d9c4b67037ee25bf2ada01585
Figo <zhaoyifei19950308@hotmail.com>
2025-03-21 16:36:26 +0800

    1

--- packages/bridge_service/__init__.py
+++ packages/bridge_service/__init__.py
@@ -7,3 +7,8 @@ def run(port=utils.PORT):
     logging.basicConfig(filename='app.log', level=log_level)
     app.run(debug=utils.DEBUG, port=port)
     app.logger.info('=============================== Bridge Service Started ===============================')
+
+def run_process(port=utils.PORT, daemon=True):
+    import multiprocessing
+    p = multiprocessing.Process(target=run, args=(port,), daemon=daemon)
+    p.start()
